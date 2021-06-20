e4c4009c0ed5b07a5620e10469e84dae9179fab0
Sandra2988 <S@ndra290988>
2021-06-21 00:06:19 +0300

    дз 3_2

--- src/main/java/lesson_3/Third.java
+++ src/main/java/lesson_3/Third.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class Third {
     public static void main(String[] args) {
-        int[] arr = {0,1,0,0};
+        int[] arr = {0, 1, 0, 0};
         Mass01(arr);
+        System.out.println();
+        MassZapolnit();
     }
 
     static void Mass01(int[] arr) {
@@ -18,4 +20,14 @@ public class Third {
             System.out.print(arr[i] + "  ");
         }
     }
+
+    static int[] MassZapolnit() {
+        int[] arr2 = new int[100];
+        for (int i = 1; i < arr2.length; i++) {
+            arr2[i] = i;
+            System.out.print(arr2[i] + "  ");
+        }
+       return arr2;
+        }
+
 }
