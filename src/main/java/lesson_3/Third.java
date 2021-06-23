5caddf04edcbe084a4cd9546e26015f2dc6fbe18
Sandra2988 <S@ndra290988>
2021-06-23 23:37:55 +0300

    дз 3

--- src/main/java/lesson_3/Third.java
+++ src/main/java/lesson_3/Third.java
@@ -1,16 +1,23 @@
 package lesson_3;
 
-import java.util.Arrays;
-
 public class Third {
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 0};
-        Mass01(arr);
+        mass01(arr);
         System.out.println();
-        MassZapolnit();
+        massZapolnit();
+        System.out.println();
+        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        massMenee6UmnozitNa2(arr2);
+        System.out.println();
+        diagonalyna1();
+        int len = 5;
+        int initialValue = 3;
+        System.out.println();
+        dlinaKajdoiYacheiki(len,initialValue);
     }
 
-    static void Mass01(int[] arr) {
+    static void mass01(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 arr[i] = 1;
@@ -21,13 +28,44 @@ public class Third {
         }
     }
 
-    static int[] MassZapolnit() {
-        int[] arr2 = new int[100];
-        for (int i = 1; i < arr2.length; i++) {
-            arr2[i] = i;
+    static int[] massZapolnit() {
+        int[] arr = new int[100];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = i;
+            System.out.print(arr[i] + "  ");
+        }
+        return arr;
+    }
+
+    static int[] massMenee6UmnozitNa2(int[] arr2) {
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr2[i] < 6) {
+                arr2[i] = arr2[i] * 2;
+            }
             System.out.print(arr2[i] + "  ");
         }
-       return arr2;
-        }
+        return arr2;
+    }
 
+    static int[][] diagonalyna1() {
+        int[][] arr3 = new int[3][3];
+        for (int i = 0; i < arr3.length; i++) {
+            for (int j = 0; j < arr3.length; j++) {
+                if (i == j) {
+                    arr3[i][j] = 1;
+                    }
+                System.out.print(arr3[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        return arr3;
+    }
+    static int[] dlinaKajdoiYacheiki(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+            System.out.print(arr[i] + "  ");
+        }
+        return arr;
+    }
 }
